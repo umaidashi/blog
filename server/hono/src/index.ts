4d43fc7e1043aadb7b478fdee5e7480507eda3ec
umaidashi <umaidashi18@gmail.com>
2024-07-06 12:24:25 +0900

    feat: turbo

--- server/hono/src/index.ts
+++ server/hono/src/index.ts
@@ -1,8 +1,16 @@
+import { Octokit } from '@octokit/rest'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
+const octokit = new Octokit()
+
 app.get('/', c => {
+  const data = octokit.repos.get({
+    owner: 'umaidashi',
+    repo: 'til'
+  })
+  console.log(data)
   return c.text('Hello Hono!')
 })
 
