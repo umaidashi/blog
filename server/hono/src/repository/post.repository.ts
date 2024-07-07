1d5becb3f9e500e4681abf0505ddf7f474c57656
umaidashi <umaidashi18@gmail.com>
2024-07-07 12:10:45 +0900

    feat: add sp

--- server/hono/src/repository/post.repository.ts
+++ server/hono/src/repository/post.repository.ts
@@ -9,14 +9,18 @@ export interface IPostRepository {
 export class PostRepository implements IPostRepository {
   async list(c: Context) {
     const octokit = new Octokit({ auth: c.env.GITHUB_TOKEN })
+
     const res = await octokit.issues.listForRepo({
       owner: c.env.GITHUB_OWNER,
       repo: c.env.GITHUB_REPO,
       state: 'closed'
     })
+
     if (res.status !== 200) throw new Error('Failed to fetch posts')
+
     const data = res.data
     const completed = data.filter(d => d.state_reason === 'completed')
+
     const posts: PostDTO[] = completed.map(d => {
       return new PostDTO(
         d.number,
