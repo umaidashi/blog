f7f13c242b4cb23202a77eae66a88da97c0d3620
umaidashi <umaidashi18@gmail.com>
2024-07-17 17:34:47 +0900

    feat: dark mode

--- views/2024-remix/app/root.tsx
+++ views/2024-remix/app/root.tsx
@@ -1,27 +1,50 @@
-import type { LinksFunction } from '@remix-run/cloudflare'
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
-import styles from './tailwind.css'
+import clsx from 'clsx'
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from 'remix-themes'
 
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
+import './styles/tailwind.css'
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare'
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react'
+import { ModeToggle } from './components/mode-toggle'
+import { themeSessionResolver } from './sessions.server'
+
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme()
+  }
+}
+// Wrap your app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
   return (
-    <html lang='ja'>
+    <ThemeProvider specifiedTheme={data.theme} themeAction='/action/set-theme'>
+      <App />
+    </ThemeProvider>
+  )
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>()
+  const [theme] = useTheme()
+  return (
+    <html lang='ja' className={clsx(theme)}>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
-        {children}
+        <ModeToggle />
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   )
-}
-
-export default function App() {
-  return <Outlet />
 }
