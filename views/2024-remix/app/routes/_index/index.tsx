ccdaf02f7753df3ae03d027037ae2d87033bc023
umaidashi <umaidashi18@gmail.com>
2024-07-19 18:00:05 +0900

    feat: rename route files

--- views/2024-remix/app/routes/_index/index.tsx
+++ views/2024-remix/app/routes/_index/index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
 import { Link } from '@remix-run/react'
-import { Header } from '~/components/header'
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,7 +14,6 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <Header />
       <Link to='/posts'>posts</Link>
     </div>
   )
