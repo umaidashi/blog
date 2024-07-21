791ede274f18962cec06555b7e73e49feac7dce3
umaidashi <umaidashi18@gmail.com>
2024-07-21 21:13:00 +0900

    feat: add react-markdown

--- views/2024-remix/app/routes/posts.$id/index.tsx
+++ views/2024-remix/app/routes/posts.$id/index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
 import { useLoaderData } from '@remix-run/react'
+import ReactMarkdown from 'react-markdown'
 import config from '~/config'
 import { getPostByIdLoader } from './loader'
 
@@ -41,18 +42,34 @@ export default function Index() {
   const post = data.data
 
   return (
-    <div>
+    <div className='flex flex-col gap-4 items-center'>
       <h1 className='text-3xl'>{post.title}</h1>
-      <p>published: {new Date(post.created_at).toLocaleDateString()}</p>
-      <p>last edited: {new Date(post.updated_at).toLocaleDateString()}</p>
-      <p>
-        {post.tags.map(tag => (
-          <span key={tag} className='border-2'>
-            {tag}
-          </span>
-        ))}
-      </p>
-      <article>{post.body}</article>
+      <div className='flex gap-4'>
+        <p>published: {new Date(post.created_at).toDateString()}</p>
+        <p>last edited: {new Date(post.updated_at).toDateString()}</p>
+        <p>
+          {post.tags.map(tag => (
+            <span
+              key={tag}
+              className='border-2 p-1 rounded-full bg-primary text-primary-foreground'>
+              {tag}
+            </span>
+          ))}
+        </p>
+      </div>
+      <article className='w-full'>
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => <h1 className='text-5xl'># {children}</h1>,
+            h2: ({ children }) => <h2 className='text-4xl'>## {children}</h2>,
+            h3: ({ children }) => <h3 className='text-3xl'>### {children}</h3>,
+            h4: ({ children }) => <h4 className='text-2xl'>#### {children}</h4>,
+            h5: ({ children }) => <h5 className='text-xl'>##### {children}</h5>,
+            h6: ({ children }) => <h6 className='text-lg'>###### {children}</h6>
+          }}>
+          {post.body}
+        </ReactMarkdown>
+      </article>
     </div>
   )
 }
