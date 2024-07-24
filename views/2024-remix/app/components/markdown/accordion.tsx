08b28cbd14c7f082441334e084c1a385c930879a
umaidashi <umaidashi18@gmail.com>
2024-07-24 18:44:46 +0900

    feat: add Accordion component

---
+++ views/2024-remix/app/components/markdown/accordion.tsx
@@ -0,0 +1,14 @@
+import type { ClassAttributes, HTMLAttributes } from 'react'
+import type { ExtraProps } from 'react-markdown'
+
+export const Details = ({
+  children
+}: ClassAttributes<HTMLDetailsElement> & HTMLAttributes<HTMLDetailsElement> & ExtraProps) => {
+  return <details className='border-2 px-4 py-4 my-1 rounded-md'>{children}</details>
+}
+
+export const Summary = ({
+  children
+}: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement> & ExtraProps) => {
+  return <summary className='text-lg font-semibold'>{children}</summary>
+}
