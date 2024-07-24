2424d71b159298df30175bedc95a6fd593fd1cec
umaidashi <umaidashi18@gmail.com>
2024-07-24 18:44:38 +0900

    fix: name

--- views/2024-remix/app/components/markdown/link.tsx
+++
@@ -1,9 +0,0 @@
-import type { AnchorHTMLAttributes, ClassAttributes } from 'react'
-import type { ExtraProps } from 'react-markdown'
-
-export const Link = ({
-  href,
-  children
-}: ClassAttributes<HTMLAnchorElement> & AnchorHTMLAttributes<HTMLAnchorElement> & ExtraProps) => {
-  return <a href={href} className='text-primary border-b-2 border-primary hover:border-none'>{children}</a>
-}
