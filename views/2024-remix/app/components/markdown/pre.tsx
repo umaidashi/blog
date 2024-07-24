367f8ace82db2b6878d76d70abe9fa9315978509
umaidashi <umaidashi18@gmail.com>
2024-07-24 21:55:59 +0900

    feat: style pre

--- views/2024-remix/app/components/markdown/pre.tsx
+++ views/2024-remix/app/components/markdown/pre.tsx
@@ -13,14 +13,21 @@ export const Pre = ({
   const childProps = isValidElement(props.children) ? props.children.props : {}
   const match = /language-(\w+)/.exec(childProps.className || '')
   const lang = match ? (match[1] ? match[1] : '') : ''
+  const name = childProps.className.split(':')[1]
   const code = highlighter.codeToHtml(String(childProps.children).replace(/\n$/, '') as string, {
     lang: lang,
     theme: 'github-dark'
   })
   return (
-    <div
-      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-      dangerouslySetInnerHTML={{ __html: code }}
-    />
+    <div className='my-4'>
+      <div className='flex justify-between items-center bg-primary text-primary-foreground py-1 px-4 rounded-t-[--radius]'>
+        <span className='text-sm'>{name}</span>
+        <span className='text-xs text-primary-foreground'>{lang}</span>
+      </div>
+      <div
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+        dangerouslySetInnerHTML={{ __html: code }}
+      />
+    </div>
   )
 }
