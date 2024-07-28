14305647928a0d9a7f4cdc0864d9889c209b4e60
umaidashi <umaidashi18@gmail.com>
2024-07-28 12:24:29 +0900

    fix: padding

--- views/2024-remix/app/components/markdown/blockquote.tsx
+++ views/2024-remix/app/components/markdown/blockquote.tsx
@@ -4,7 +4,7 @@ export const Blockquote = ({
   ...props
 }: ClassAttributes<HTMLQuoteElement> & HTMLAttributes<HTMLQuoteElement>) => {
   return (
-    <blockquote className='border-l-2 border-primary bg-secondary text-secondary-foreground px-4 my-2 rounded-r-[--radius]'>
+    <blockquote className='border-l-2 border-primary bg-secondary text-secondary-foreground p-4 my-2 rounded-r-[--radius]'>
       {props.children}
     </blockquote>
   )
