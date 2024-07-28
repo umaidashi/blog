be8319ca228e078e40c34d9f43a5ae41a4ee1690
umaidashi <umaidashi18@gmail.com>
2024-07-28 12:24:34 +0900

    fix: bg-color

--- views/2024-remix/app/components/markdown/code.tsx
+++ views/2024-remix/app/components/markdown/code.tsx
@@ -1,5 +1,5 @@
 import type { ClassAttributes, HTMLAttributes } from 'react'
 
 export const Code = ({ ...props }: ClassAttributes<HTMLElement> & HTMLAttributes<HTMLElement>) => {
-  return <code className='bg-gray-100 text-red-500 p-1 text-sm rounded'>{props.children}</code>
+  return <code className='bg-secondary text-red-500 p-1 text-sm rounded'>{props.children}</code>
 }
