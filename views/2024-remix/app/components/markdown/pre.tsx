52922d8fa643cf673191996921577a6c696b5507
umaidashi <umaidashi18@gmail.com>
2024-07-28 17:41:32 +0900

    feat: Promise

--- views/2024-remix/app/components/markdown/pre.tsx
+++ views/2024-remix/app/components/markdown/pre.tsx
@@ -1,22 +1,33 @@
-import { type ClassAttributes, type HTMLAttributes, isValidElement } from 'react'
-import { bundledLanguages, bundledThemes, createHighlighter } from 'shiki'
-
-const highlighter = await createHighlighter({
-  themes: Object.keys(bundledThemes),
-  langs: Object.keys(bundledLanguages)
-})
+import {
+  type ClassAttributes,
+  type HTMLAttributes,
+  isValidElement,
+  useEffect,
+  useState
+} from 'react'
+import { codeToHtml } from 'shiki'
 
 export const Pre = ({
   ...props
 }: ClassAttributes<HTMLPreElement> & HTMLAttributes<HTMLPreElement>) => {
+  const [code, setCode] = useState<string>('')
+
   const childProps = isValidElement(props.children) ? props.children.props : {}
   const match = /lang-(\w+)/.exec(childProps.className || '')
   const lang = match ? (match[1] ? match[1] : '') : ''
   const name = childProps.className.split(':')[1]
-  const code = highlighter.codeToHtml(String(childProps.children).replace(/\n$/, '') as string, {
-    lang: lang,
-    theme: 'github-dark'
-  })
+
+  useEffect(() => {
+    const genCode = async () => {
+      const code = await codeToHtml(String(childProps.children).replace(/\n$/, '') as string, {
+        lang: lang,
+        theme: 'github-dark'
+      })
+      setCode(code)
+    }
+
+    genCode()
+  }, [childProps.children, lang])
   return (
     <div className='my-4'>
       <div className='flex justify-between items-center bg-primary text-primary-foreground py-1 px-4 h-8 rounded-t-[--radius]'>
