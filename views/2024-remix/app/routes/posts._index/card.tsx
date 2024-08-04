d9e41fa28b6abccf9bd163e969a0a16ae5dc2d06
umaidashi <umaidashi18@gmail.com>
2024-08-04 17:28:30 +0900

    feat: post list style

---
+++ views/2024-remix/app/routes/posts._index/card.tsx
@@ -0,0 +1,60 @@
+import type { SerializeFrom } from '@remix-run/cloudflare'
+import { Link } from '@remix-run/react'
+import { Platforms } from '@server/hono/src/dto/post.dto'
+import { IconContext } from 'react-icons'
+import { HiExternalLink } from 'react-icons/hi'
+import { SiZenn } from 'react-icons/si'
+import { TbAlignBoxLeftTop } from 'react-icons/tb'
+import { tv } from 'tailwind-variants'
+import type { getPostsLoaderType } from './loader'
+
+type props = SerializeFrom<getPostsLoaderType['data'][0]>
+
+const card = tv({
+  slots: {
+    base: 'p-4 rounded-[--radius] transition hover:opacity-60',
+    title: 'text-lg font-bold',
+    date: 'text-sm text-gray-500',
+    wrapper: 'flex items-center gap-2'
+  }
+})
+const { base, title, date, wrapper } = card()
+
+export function Card({
+  post
+}: {
+  post: props
+}) {
+  if (post.platform === Platforms.Zenn) {
+    return <ZennPostCard post={post} />
+  }
+  return (
+    <li className={base()}>
+      <Link to={`/posts/${post.id}`} className=''>
+        <div className={wrapper()}>
+          <TbAlignBoxLeftTop />
+          <h2 className={title()}>{post.title}</h2>
+        </div>
+        <p className={date()}>{new Date(post.created_at).toLocaleDateString('ja-JP')}</p>
+      </Link>
+    </li>
+  )
+}
+
+function ZennPostCard({ post }: { post: props }) {
+  if (!post.zennObj) return null
+  return (
+    <li className={base()}>
+      <Link to={`https://zenn.dev${post.zennObj.path}`} className=''>
+        <div className={wrapper()}>
+          <IconContext.Provider value={{ color: '#3EA8FF' }}>
+            <SiZenn />
+          </IconContext.Provider>
+          <h2 className={title()}>{post.title}</h2>
+          <HiExternalLink />
+        </div>
+        <p className={date()}>{new Date(post.created_at).toLocaleDateString('ja-JP')}</p>
+      </Link>
+    </li>
+  )
+}
--- views/2024-remix/app/routes/posts._index/index.tsx
+++ views/2024-remix/app/routes/posts._index/index.tsx
@@ -1,6 +1,8 @@
 import type { MetaFunction } from '@remix-run/cloudflare'
-import { Link, useLoaderData } from '@remix-run/react'
+import { useLoaderData } from '@remix-run/react'
+import { tv } from 'tailwind-variants'
 import config from '~/config'
+import { Card } from './card'
 import { getPostsLoader } from './loader'
 
 export const meta: MetaFunction = () => {
@@ -13,6 +15,15 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+const postList = tv({
+  slots: {
+    header: 'text-2xl font-bold py-8',
+    wrapper: 'flex flex-col gap-4'
+  }
+})
+
+const { header, wrapper } = postList()
+
 export const loader = getPostsLoader
 
 export default function Index() {
@@ -20,14 +31,10 @@ export default function Index() {
 
   return (
     <div>
-      <h1 className='text-3xl'>Posts</h1>
-      <ul className='list-disc mt-4 pl-6 space-y-2'>
+      <h1 className={header()}>投稿一覧</h1>
+      <ul className={wrapper()}>
         {data.data.map(post => (
-          <li key={post.id}>
-            <Link to={`/posts/${post.id}`}>
-              {post.platform} : {post.title} : {post.created_at}
-            </Link>
-          </li>
+          <Card key={post.id} post={post} />
         ))}
       </ul>
     </div>
