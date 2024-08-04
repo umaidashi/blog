a08608d88c6de697f3a678837dd9c7da89449553
umaidashi <umaidashi18@gmail.com>
2024-08-04 18:12:21 +0900

    feat: crate Profile page

---
+++ views/2024-remix/app/routes/profile.tsx
@@ -0,0 +1,11 @@
+import { Header } from '~/components/header'
+import PageTitle from '~/components/page_title'
+
+export default function Profile() {
+  return (
+    <div>
+      <Header />
+      <PageTitle title='oidon. - umaidashi' />
+    </div>
+  )
+}
