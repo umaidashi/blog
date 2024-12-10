f59210e33c634b54af34a15f504468412b33dced
umaidashi <umaidashi18@gmail.com>
2024-12-10 18:49:57 +0900

    feat: heading style

--- views/2024-remix/app/components/markdown/heading.tsx
+++ views/2024-remix/app/components/markdown/heading.tsx
@@ -8,35 +8,35 @@ const HeadingPrefix = ({ num }: { num: number }) => {
 export const H1 = ({
   ...props
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h1 className='text-5xl'><HeadingPrefix num={1} /> {props.children}</h1>
+  return <h1 className='text-4xl font-bold'><HeadingPrefix num={1} /> {props.children}</h1>
 }
 
 export const H2 = ({
   ...props
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h2 className='text-4xl'><HeadingPrefix num={2} /> {props.children}</h2>
+  return <h2 className='text-3xl font-bold'><HeadingPrefix num={2} /> {props.children}</h2>
 }
 
 export const H3 = ({
   ...props
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h3 className='text-3xl'><HeadingPrefix num={3} /> {props.children}</h3>
+  return <h3 className='text-2xl font-bold'><HeadingPrefix num={3} /> {props.children}</h3>
 }
 
 export const H4 = ({
   children
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h4 className='text-2xl'><HeadingPrefix num={4} /> {children}</h4>
+  return <h4 className='text-xl font-bold'><HeadingPrefix num={4} /> {children}</h4>
 }
 
 export const H5 = ({
   children
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h5 className='text-xl'><HeadingPrefix num={5} /> {children}</h5>
+  return <h5 className='text-lg font-bold'><HeadingPrefix num={5} /> {children}</h5>
 }
 
 export const H6 = ({
   children
 }: ClassAttributes<HTMLHeadingElement> & HTMLAttributes<HTMLHeadingElement>) => {
-  return <h6 className='text-lg'><HeadingPrefix num={6} /> {children}</h6>
+  return <h6 className='text-base font-bold'><HeadingPrefix num={6} /> {children}</h6>
 }
