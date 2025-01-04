9828d51e1406dc963e540b852c045fdb8c4be949
umaidashi <umaidashi18@gmail.com>
2025-01-05 00:00:39 +0900

    chore: packages

--- views/2024-remix/vite.config.ts
+++ views/2024-remix/vite.config.ts
@@ -4,9 +4,11 @@ import {
 } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import wasm from 'vite-plugin-wasm'
 
 export default defineConfig({
   plugins: [
+    wasm(),
     remixCloudflareDevProxy(),
     remix({
       // future: {
