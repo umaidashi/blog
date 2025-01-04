1273de18ff7358867f6594981c12981fcff305f7
umaidashi <umaidashi18@gmail.com>
2025-01-04 10:51:40 +0900

    feat(links): add homepage link to app configuration

--- views/2024-remix/app/config/index.ts
+++ views/2024-remix/app/config/index.ts
@@ -3,13 +3,16 @@ import data from '../../package.json'
 export default {
   app: {
     name: data.name,
-    sitename: data.author.name,
+    sitename: `${data.author.name} のブログ`,
     author: data.author.name,
     description: data.description
   },
   links: {
     github: data.author.github,
     x: data.author.x,
-    zenn: data.author.zenn
+    x_name: data.author.x_name,
+    zenn: data.author.zenn,
+    homepage: data.homepage,
+    ogptop: 'https://chietto.yu-ta-9.com/ogp.png'
   }
 }
